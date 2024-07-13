7dbaeaf509c7f79849b6e6d216a5984ef22270c2
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2024-07-13 14:17:39 +0200

    Update to loginSv.bat

--- system/loginSv.bat
+++ system/loginSv.bat
@@ -1,1 +1,69 @@
+@echo off
 
+:main
+cls
+    echo.
+    echo    ╔══════════════════════════════════╗
+    echo    ║           Please login           ║
+    echo    ╠══════════════════════════════════╣
+    echo    ║                                  ║
+    echo    ║             ◆ Root               ║
+    echo    ║             ◆ Guest              ║
+    echo    ║                                  ║
+    echo    ╚══════════════════════════════════╝
+    echo.
+    set /p "sys.usern=usern >> "
+    if exist "..\home\!sys.usern!" (
+        call "..\home\!sys.usern!\userData.bat"
+        goto :password_chk
+        call system\terminal.bat
+    ) else (
+        goto :fn_invalid
+    )
+
+goto :main
+
+:fn_invalid
+        cls
+        echo.
+        echo    ╔══════════════════════════════════╗
+        echo    ║           Please login           ║
+        echo    ╠══════════════════════════════════╣
+        echo    ║                                  ║
+        echo    ║         Invalid username         ║
+        echo    ║                                  ║
+        echo    ╚══════════════════════════════════╝
+        echo.
+        pause > nul
+        goto :main
+
+:password_chk
+    echo.
+    echo    ╔══════════════════════════════════╗
+    echo    ║          Enter password          ║
+    echo    ╠══════════════════════════════════╣
+    echo    ║                                  ║
+    echo    ║      Enter password to login     ║
+    echo    ║                                  ║
+    echo    ╚══════════════════════════════════╝
+    echo.
+    set /p "sys.userpasskey=>>"
+    if "sys.userpasskey"=="user.passkey" (
+        call system\terminal.bat
+    ) else (
+        goto :fn_invalid2
+    )
+
+:fn_invalid2
+        cls
+        echo.
+        echo    ╔══════════════════════════════════╗
+        echo    ║           Please login           ║
+        echo    ╠══════════════════════════════════╣
+        echo    ║                                  ║
+        echo    ║         Invalid password         ║
+        echo    ║                                  ║
+        echo    ╚══════════════════════════════════╝
+        echo.
+        pause > nul
+        goto :password_chk
