0128f5119e3d9df300f7336c091acc5f30728105
Gorciu <gorciuyt@gmail.com>
2024-07-13 20:37:11 +0200

    Some changes

--- system/terminal.bat
+++ system/terminal.bat
@@ -6,7 +6,7 @@ cls
 set "user.input=placeholder"
 set /p "user.input=!os.usern!@!os.pcname! | !os.path! >> "
 if "!user.input!"=="placeholder" (
-    call "system\messege.bat" redmsg "Please enter command"
+    call "system\message.bat" redmsg "Please enter command"
 ) else (
     for /f "tokens=1,2,3,4,5,6,7,8,9,10" %%A in ("!user.input!") do (
         if "%%A"=="diskinfo" (
